9e3a0205444cb308c6a0bdd74357f0b9e8de4475
Paresh-29 <pareshkumarbarick99@gmail.com>
2024-03-25 22:46:46 +0530

    Remove unused code and update routing in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,18 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { SignIn } from "./pages/SignIn";
 import { SignUp } from "./pages/SignUp";
 import { SendMoney } from './pages/SendMoney';
 import { Dashboard } from './pages/Dashboard';
-import useGetUser from './components/hooks/useGetUser';
 
 function App() {
-const {user, verify} = useGetUser();
-
-
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<SignUp/>}/>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        {verify === true ? (
-          <>
-              <Route path='/dashboard' element={<Dashboard/>}/>
-              <Route path='/send' element={<SendMoney />}/>
-          </>
-        ): (<Navigate to= "/signin"/>)
-      }
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/send' element={<SendMoney />}/>
       </Routes>
     </BrowserRouter>
   );
